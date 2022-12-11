1b17dfd4e16a43a8af6bc544aed11596c268e6af
davidlam-craftsair <davidlam0514@gmail.com>
2022-12-11 20:20:44 +0800

    added remove and add todo functions

--- index.js
+++ index.js
@@ -13,16 +13,37 @@ function App() {
       isCompleted: false,
     }
   ]);
-  return (<>
-    {todos.map((todo, i) => <div class='app'
-      key={i}>
-      <div class='todo-list'>
-        <div class='todo'>
-          {todo.text}
-        </div>
-      </div>
-    </div>)}
-  </>
+
+  const [value, setValue] = React.useState(''); // value for input
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(`value is ${value}`);
+    let tmp = [...todos, { text: value, isCompleted: false }];
+    setTodos(tmp);
+    onToDoAdded();
+  }
+
+  const onToDoAdded = () => {
+    // reset value to empty, should display placeholder
+    setValue('');
+  }
+
+  return (<div class='app'>
+    <div class="todo-list">
+      {todos.map((todo, i) => <div class="todo" key={i}>
+        <button>-</button><div class="todo-text">{todo.text}</div>
+      </div>)}
+      <form onSubmit={handleSubmit}>
+        <input type='text'
+          className="input"
+          value={value}
+          placeholder="Add ToDo ..."
+          onChange={e => setValue(e.target.value)} // onChange
+        />
+      </form>
+    </div>
+  </div>
   );
 }
 
